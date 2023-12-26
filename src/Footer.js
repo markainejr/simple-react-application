2b88d91bf775d65a641cf5b6d5cd20658f25448c
Mark Aine <ainemarkjr.com>
2023-12-26 23:06:59 +0300

    made changes to the footer

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,7 @@ import React from "react";
 export default function Footer(){
     return(
     <footer>
-         <small>"@ 20xx aine development. All rights reserved."</small>
+         <small>" &copy; 2023  ainemark. All rights reserved."</small>
     </footer>
     )
 }
